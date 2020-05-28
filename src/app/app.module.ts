db1a2826763ddd6aea25850c325ba6bfe9e13240
HemanthKumarLavu <hemanthkumar.lavu@vasudhaika.net>
2020-05-28 23:03:39 +0530

    created button for opening a dialoge on task ceration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeViewComponent } from './views/home-view/home-view.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TaskTileComponent } from './components/task-tile/task-tile.component';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
+import { CreateTaskFormComponent } from './components/create-task-form/create-task-form.component';
 
 // Modules
 import { AppRoutingModule } from './app-routing.module';
@@ -13,13 +15,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeViewComponent,
     HeaderComponent,
-    TaskTileComponent
+    TaskTileComponent,
+    CreateTaskComponent,
+    CreateTaskFormComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +36,9 @@ import { MatExpansionModule } from '@angular/material/expansion';
     // Material modules
     MatToolbarModule,
     MatButtonModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
